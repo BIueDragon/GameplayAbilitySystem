405d722fb904e1fa026590a969a19bb50ec1544a
GuTech <adriangutowicz99@gmail.com>
2024-05-12 00:46:16 +0200

    Safe

--- Source/GAS/Private/MainCharacterBase.cpp
+++ Source/GAS/Private/MainCharacterBase.cpp
@@ -7,28 +7,14 @@
 AMainCharacterBase::AMainCharacterBase()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 }
 
-// Called when the game starts or when spawned
 void AMainCharacterBase::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
-// Called every frame
-void AMainCharacterBase::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
-// Called to bind functionality to input
-void AMainCharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-}
 
